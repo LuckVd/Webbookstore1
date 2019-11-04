138683365a21153b80bcf12c4359466985839edb
LuckVd <fjw7478@qq.com>
2019-11-04 20:22:25 +0800

    实现了shop界面翻页的功能

--- shopapp/static/js/shop.js
+++ shopapp/static/js/shop.js
@@ -42,35 +42,78 @@ jQuery( document ).ready(function() {
         dataType: "JSON",
         success: function (data) {
             var author_len = data.name.length;
-            console.log(author_len);
             for(var i=0;i<author_len;i++) {
              $('#Author').append('<li><a a href="#">' + data.name[i] + '</a></li> ');
           }
 
         },
         error: function () {
-            console.log("failed")
+            console.log("failed");
         }
     });
+
+    function display_bolist(data,i) {
+        $("#book_list").find("div.product-box").each(function(){
+                if(typeof(data.pic_src[i])=="undefined"){$(this).hide()}
+                else{
+                    $(this).show();
+                    $(this).find("img").attr("src",data.pic_src[i]);
+                $(this).find("h5").find("a").text(data.name[i])
+                $(this).find("div.product-detail").find("p").text(data.description[i]);
+                $(this).find("strong").text(data.price[i]);
+                }
+
+                i++;
+        })
+    }
+
     //获得书籍信息
+    var booklist;
+    var nowpage=1;
+    var maxpage;
     $.ajax({
         type: 'post',
         url: 'book_list',
         dataType: "JSON",
         success: function (data) {
-             var num_of_books=0
-             $("#book_list").find("div.product-box").each(function(){
-                 $(this).find("img").attr("src",data.pic_src[num_of_books]);
-                 $(this).find("h5").find("a").text(data.name[num_of_books])
-                 $(this).find("div.product-detail").find("p").text(data.description[num_of_books])
-                 $(this).find("strong").text(data.price[num_of_books])
-                 num_of_books++;
-             })
+            booklist = data
+            var num_of_books = data.name.length;
+             maxpage = Math.ceil(num_of_books/9)+1
+            //自动生成页码
+             $('ul.pagination').append('<li><Button  class="c_page" id="page_pre"><a aria-label="Previous">'+'Prev'+'</a></Button></li> ');
+            for(var i=1;i<maxpage;i++)
+                {
+                 $('ul.pagination').append('<li><Button class="c_page" id="page'+i+'"><a>' + i + '</a></Button></li> ');
+                 }
+                $('ul.pagination').append('<li><button  class="c_page" id="page_next" ><a aria-label="Next">'+'Next'+'</a></button></li> ');
 
+            var i=0;
+            display_bolist(booklist,i)
         },
         error: function () {
             console.log("book_detail_failed")
         }
     });
+
+
+    $("ul.pagination").on("click",".c_page",function(){
+        var id=$(this).attr('id')
+        if(id=="page_pre")
+        {
+            if(nowpage>1){nowpage=nowpage-1;}
+            else{alert("已经是第一页");}
+        }
+        else if(id=="page_next")
+        {
+            if(nowpage<maxpage-1){nowpage=nowpage+1}
+             else{alert("已经是最后一页");}
+        }
+        else{
+            nowpage=id.replace(/[^0-9]/ig,"")
+          }
+        display_bolist(booklist,(nowpage-1)*9)
+     });
+
+
 
 })
